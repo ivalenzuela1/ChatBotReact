747d06def6d70a1c9bc84f22eb4ede464453b294
Ignacio Valenzuela <ignaciovalen1@gmail.com>
2023-05-06 13:26:12 -0700

    Modify /update api to use the openAi library to summarize attachment

--- react-node-app/client/src/App.js
+++ react-node-app/client/src/App.js
@@ -66,7 +66,46 @@ function App() {
   );
 }
 
-function FileUploadSingleReturnText() {
+function FileUploadSingle() {
+  const [file, setFile] = useState(null);
+  const [text, setText] = useState("");
+
+  const handleChange = (event) => {
+    setFile(event.target.files[0]);
+  };
+
+  const handleSubmit = async (event) => {
+    console.log("handleSubmit clicked");
+    event.preventDefault();
+    const formData = new FormData();
+    formData.append("pdf", file);
+
+    try {
+      const response = await axios.post("/upload", formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      });
+
+      console.log("::response.data:::");
+      console.log(response.data);
+      setText(response.data.message);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input type="file" name="pdf" onChange={handleChange} />
+        <button type="submit">Submit</button>
+      </form>
+      {text && <p>{text}</p>}
+    </div>
+  );
+}
+
+function FileUploadSingleReturnText1() {
   const [file, setFile] = useState(null);
   const [text, setText] = useState("");
 
@@ -95,45 +134,6 @@ function FileUploadSingleReturnText() {
     }
   };
   return text;
-}
-
-function FileUploadSingle() {
-  const [file, setFile] = useState(null);
-  const [text, setText] = useState("");
-
-  const handleChange = (event) => {
-    setFile(event.target.files[0]);
-  };
-
-  const handleSubmit = async (event) => {
-    console.log("handleSubmit clicked");
-    event.preventDefault();
-    const formData = new FormData();
-    formData.append("pdf", file);
-
-    try {
-      const response = await axios.post("/upload", formData, {
-        headers: {
-          "Content-Type": "multipart/form-data",
-        },
-      });
-
-      console.log("::response.data:::");
-      console.log(response.data);
-      setText(response.data);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-  return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <input type="file" name="pdf" onChange={handleChange} />
-        <button type="submit">Submit</button>
-      </form>
-      {text && <p>{text}</p>}
-    </div>
-  );
 }
 
 function FileUploadSingle2() {
